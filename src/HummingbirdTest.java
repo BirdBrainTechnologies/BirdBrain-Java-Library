585297ef74b004312ae4baf2c1354a8fe06e9457
bambi <bambi@birdbraintechnologies.com>
2018-11-20 16:26:34 -0500

    Trying to fix dirty files
    This reverts commit 45f1849689974842627233e2087b8018f4ac0c25.

--- src/HummingbirdTest.java
+++ src/HummingbirdTest.java
@@ -1,9 +1,8 @@
 public class HummingbirdTest {
     public static void main(String[] args) {
-        //Hummingbird myBit = new Hummingbird("A");
-        Microbit testM = new Microbit("B");
+        Hummingbird myBit = new Hummingbird("A");
         
-       /* for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
         	myBit.setLED(1, 100);
         	myBit.pause(1);
         	myBit.setLED(1, 0);
@@ -11,6 +10,6 @@ public class HummingbirdTest {
         }
         
         myBit.stopAll();
-        myBit.disconnect();*/
+        myBit.disconnect();
     }
 }
